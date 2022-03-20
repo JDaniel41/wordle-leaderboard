8f889cab9545fd4675a47c764c6c19c52958ac18
Jonathan Daniel <gcagrad19@gmail.com>
2022-03-20 12:47:49 -0400

    Initial Commit

---
+++ server/index.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.json());
+
+let table = [];
+
+function addToTable(rowOutput, name, score, gameNum) {
+    // Check if character in rowOutput is a black square emoji
+    table.push({
+        timestamp: Date.now(),
+        name: name,
+        score: score,
+        gameNum: gameNum,
+        rowOutput: rowOutput,
+    });
+}
+
+app.post("/api/addScore", (req, res) => {
+    gameOutput = req.body.output;
+    nameOutput = req.body.name;
+    pinNum = req.body.pinNum;
+
+    if (!gameOutput || !nameOutput || !pinNum) {
+        res.status(400).send("Bad Request");
+        return;
+    }
+
+    if (pinNum != "1234") {
+        res.status(401).send("Unauthorized");
+        return;
+    }
+    // Read the first number in the body
+    processedOutput = gameOutput
+        .split(" ")
+        .slice(0, 3)
+        .concat(gameOutput.split("\n").slice(2));
+    processedOutput[2] = processedOutput[2].slice(0, 3);
+
+    let gameNum = processedOutput[1];
+    let numLinesUsed = processedOutput[2][0];
+
+    addToTable(processedOutput.slice(3), nameOutput, numLinesUsed, gameNum);
+
+    res.send(table);
+});
+
+/*
+const {
+    DynamoDBClient,
+    ListTablesCommand,
+} = require("@aws-sdk/client-dynamodb");
+
+const client = new DynamoDBClient({
+    region: "us-east-1",
+});
+
+(async () => {
+    const client = new DynamoDBClient({ region: "us-west-2" });
+    const command = new ListTablesCommand({});
+    try {
+        const results = await client.send(command);
+        console.log("DONE!");
+        console.log(results.TableNames.join("\n"));
+    } catch (err) {
+        console.log("Whoops");
+        console.error(err);
+    }
+})();*/
+
+app.listen(3000, () => {
+    console.log("Server started on port 3000");
+});
