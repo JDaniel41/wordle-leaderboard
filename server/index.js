99b1f047715a8a48926351fddeb69b7c008a67e2
Jonathan Daniel <gcagrad19@gmail.com>
2022-03-22 20:27:34 -0400

    Updates 1

--- server/index.js
+++ server/index.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const cors = require("cors");
 
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(express.static(__dirname + "/public"));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 let table = [];
 
